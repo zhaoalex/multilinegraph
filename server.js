09207f6c22717229a0d6c83329cda6b22adbc9f5
Alex Zhao <alexzhao21@gmail.com>
2018-08-08 12:59:12 +0530

    heroku 4

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/multilinegraph'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-})
+app.get('/*', function(req,res) {
 
-const port = process.env.PORT || '3001';
-app.set('port', port);
+res.sendFile(path.join(__dirname,'/dist/multilinegraph/index.html'));
+});
 
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
